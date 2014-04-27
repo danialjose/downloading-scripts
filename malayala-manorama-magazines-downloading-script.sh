2f90b80b7e091e71aea8fb24156cf38f8088a03c
Abhilash Divakaran <abhilashd@users.noreply.github.com>
2014-04-26 17:15:01 -0700

    Update malayala-manorama-magazines-downloading-script.sh
    Fixed downloading of Malayala Manorama weekly.

--- malayala-manorama-magazines-downloading-script.sh
+++ malayala-manorama-magazines-downloading-script.sh
@@ -7,7 +7,7 @@
 #As always /// Its free to use...
 
 #Get user to select edition
-edition_choice=([0]=MMFast [1]=MMKarshakasree [2]=MMSamp [3]=MMVanitha [4]=MMMan [5]=MMWeek [6]=MMBhasha [7]=MMWeekly )
+edition_choice=([0]=MMFast [1]=MMKarshakasree [2]=MMSamp [3]=MMVanitha [4]=MMMan [5]=MMWeek [6]=MMBhasha [7]=%5CMMWeekly )
 edition_name=([0]=MMFast [1]=MMKarshakasree [2]=MMSamp [3]=MMVanitha [4]=MMMan [5]=MMWeek [6]=MMBhasha [7]=MMWeekly )
 echo "MM Magazines are"
 echo "-------------------------------------------------"
@@ -18,7 +18,7 @@ echo "3. Vanitha"
 echo "4. The Man"
 echo "5. The Week (Error with default date -01, change _01_ to _15_ or _30_ manualy)"
 echo "6. Bashaposhini (Not working)"
-echo "7. Manorama Weekly (Not working)"
+echo "7. Manorama Weekly"
 echo "-------------------------------------------------"
 while true; do
     read -p "Enter magazine you wish to selec[0-3]: " ed
@@ -29,6 +29,10 @@ while true; do
         * ) echo "Please select the correct numeric serial.";;
     esac
 done
+
+url_prefix=([7]=http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[$ed]}/`date -dnext-saturday +%Y`/`date -dnext-saturday +%m`/`date -dnext-saturday +%d`/${edition_choice[ed]}_`date -dnext-saturday +%Y`_`date -dnext-saturday +%m`_`date -dnext-saturday +%d`)
+folder_format=([7]=`date -dnext-saturday +%Y``date -dnext-saturday +%m``date -dnext-saturday +%d`)
+
 #spider the selected edition using wget to estimate number of pages
 #define max incremental page limit
 max_spider=200
@@ -40,7 +44,11 @@ echo "Estimating number of pages in magazine edition"
       _PR='_PR'
       pageno=`printf "%03d" $j`  
       echo "Searching for Page $pageno"
-      I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno_PR.jpg"
+      if [ -z ${url_prefix[$ed]} ]; then
+            I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno$_PR.jpg"
+      else
+            I_FILE="${url_prefix[$ed]}_${pageno}${_PR}.jpg"
+      fi
  debug=`wget --spider $I_FILE 2>&1`
  echo $debug
       if [[ $debug =~ .*link!!!.* ]]
@@ -54,29 +62,11 @@ clear
 npages_A=$j
 echo "Estimating number of pages in magazine edition supplement"
 
-
-
-#start spider for newapaper supplement
- for ((  j = 1 ;  j <= $max_spider;  j++  ))
-    do
-      #prepend zero to single digits
-      pageno=`printf "%03d" $j`  
-      echo "Searching for Page $pageno"
-      I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno$_PR.jpg"
- debug=`wget --spider $I_FILE 2>&1`
- 
- echo $debug
-      if [[ $debug =~ .*link!!!.* ]]
-      then
-      break
-      fi
-done
-clear
-#decrement counter
-(( j = j - 1 ))
-npages_B=$j
-
-ty_dir="$HOME/Desktop/${edition_choice[ed]}`date +%m``date +%y`"
+if [ -z ${folder_format[$ed]} ]; then
+    ty_dir="$HOME/Desktop/${edition_name[ed]}`date +%m``date +%y`"
+else
+    ty_dir="$HOME/Desktop/${edition_name[ed]}${folder_format[$ed]}"
+fi
 #mkdir to store individual pages
 mkdir $ty_dir
 echo "Please be patient..Bandwidth intensive operation starts..;-)"
@@ -87,23 +77,16 @@ echo "Downloading Main Paper .. total $npages_A pages"
       pageno=`printf "%03d" $i`  
       echo "Downloading Page $pageno"
       O_FILE="$ty_dir/A$pageno.jpg"
-      I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno$_PR.jpg"
+      if [ -z ${url_prefix[$ed]} ]; then
+            I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno$_PR.jpg"
+      else
+            I_FILE="${url_prefix[$ed]}_${pageno}${_PR}.jpg"
+      fi
+
       wget -q -O $O_FILE $I_FILE 
       
     done
     
-echo "Downloading Supplement .. total $npages_B pages"
-
-  for ((  i = 1 ;  i <= npages_B;  i++  ))
-    do
-      #prepend zero to single digits
-      pageno=`printf "%03d" $i`  
-      echo "Downloading Page $pageno"
-      O_FILE="$ty_dir/B$pageno.jpg"
-      I_FILE="http://eweekly.manoramaonline.com/ManoramaLibrary/${edition_choice[ed]}/`date +%Y`/`date +%m`/01/${edition_choice[ed]}_`date +%Y`_`date +%m`_01_$pageno$_PR.jpg"
-      wget -q -O $O_FILE $I_FILE 
-      
-    done
 echo "All downloads are finished, Please close this window and enjoy reading!"
 
 
